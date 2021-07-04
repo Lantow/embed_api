91da88d683afe5391c35b441df8c71c84021b063
M.Lantow <mrlantow@gmail.com>
2021-07-04 18:18:28 +0200

    Should work noW

--- emb_api/bootup_emb_api.py
+++ emb_api/bootup_emb_api.py
@@ -1,25 +1,49 @@
-from flask import Flask, session, render_template, request, redirect, url_for, jsonify, send_from_directory, abort
-
+from flask import Flask, request, jsonify
+from embed_and_reduce.emb_and_reduce import ReduceDim, CleanAndEmb, EmbAndReduce
 app = Flask(__name__)
 
 @app.route('/embed_string', methods=['POST'])
 def embed_string():
     if request.method == 'POST':
         post = request.get_json()
-        embedding = None # TODO: do the embedding here
-        return embedding
+                
+        try:
+            CE = CleanAndEmb(post["search_string"])
+            CE.clean_and_embed()
+            embedding = CE.embedding
+        except Exception as E:
+            raise E
+        
+        return jsonify({"emb":embedding.tolist()})
 
 @app.route('/dim_reduce', methods=['POST'])
 def dim_reduce():
     if request.method == 'POST':
         post = request.get_json()
-        reduced_emb = None # TODO: do the dimensionality reduction here
-        return reduced_emb
+        
+        try:
+            RD = ReduceDim(post["embedding"])
+            RD.reduce_dim()
+            reduced_emb = RD.emb100
+        except Exception as E:
+            raise E
+        
+        return jsonify({"emb100":reduced_emb.tolist()})
     
 @app.route('/dim_reduce_and_emb', methods=['POST'])
 def embed_and_reduce():
         if request.method == 'POST':
             post = request.get_json()
-            embedding = None # TODO: do the embedding here
-            reduced_emb = None # TODO: do the dimensionality reduction here
-            return reduced_emb
+            
+        try:
+            ER = EmbAndReduce(post["search_string"])
+            ER.clean_and_embed()
+            ER.reduce_dim()
+            reduced_emb = ER.emb100
+        except Exception as E:
+            raise E
+        
+        return jsonify({"emb100":reduced_emb.tolist()})
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug='True')
