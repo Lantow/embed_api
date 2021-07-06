362f6aabbf4e2f715294769a4826368eda06526a
Jonas Høgh Jeppesen <jha@peachestheapp.com>
2021-07-06 21:12:09 +0200

    startet dvc på embed_api repo

--- emb_api/bootup_emb_api.py
+++ emb_api/bootup_emb_api.py
@@ -1,16 +1,18 @@
 from flask import Flask, request, jsonify
-from embed_and_reduce.emb_and_reduce import ReduceDim, CleanAndEmb, EmbAndReduce
+from embed_and_reduce.emb_and_reduce import EmbAndReduce
+import numpy as np
+
 app = Flask(__name__)
+
+EmbedAndReduce = EmbAndReduce()
 
 @app.route('/embed_string', methods=['POST'])
 def embed_string():
     if request.method == 'POST':
         post = request.get_json()
-                
+
         try:
-            CE = CleanAndEmb(post["search_string"])
-            CE.clean_and_embed()
-            embedding = CE.embedding
+            embedding = EmbedAndReduce.clean_and_embed(post["search_string"])
         except Exception as E:
             raise E
         
@@ -20,30 +22,26 @@ def embed_string():
 def dim_reduce():
     if request.method == 'POST':
         post = request.get_json()
-        
+        embedding = np.asarray(post["embedding"])
+
         try:
-            RD = ReduceDim(post["embedding"])
-            RD.reduce_dim()
-            reduced_emb = RD.emb100
+            reduced_emb = EmbedAndReduce.dim_reduce(embedding)
         except Exception as E:
             raise E
-        
+
         return jsonify({"emb100":reduced_emb.tolist()})
     
-@app.route('/dim_reduce_and_emb', methods=['POST'])
+@app.route('/embed_and_reduce', methods=['POST'])
 def embed_and_reduce():
         if request.method == 'POST':
             post = request.get_json()
-            
+
         try:
-            ER = EmbAndReduce(post["search_string"])
-            ER.clean_and_embed()
-            ER.reduce_dim()
-            reduced_emb = ER.emb100
+            reduced_emb = EmbedAndReduce.embed_and_reduce(post["search_string"])
         except Exception as E:
             raise E
         
         return jsonify({"emb100":reduced_emb.tolist()})
-    
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug='True')
