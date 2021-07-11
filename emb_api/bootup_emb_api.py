1a0d58d40f86af51345d7d783e3d0d1bfeb6d96c
M.Lantow <mrlantow@gmail.com>
2021-07-11 14:45:24 +0200

    now with an extra endpoint for sentance tokenization

--- emb_api/bootup_emb_api.py
+++ emb_api/bootup_emb_api.py
@@ -1,20 +1,35 @@
+import numpy as np
 from flask import Flask, request, jsonify
-from embed_and_reduce.emb_and_reduce import ReduceDim, CleanAndEmb, EmbAndReduce
+from embed_and_reduce.emb_and_reduce import ReduceDim, CleanAndEmb, EmbAndReduce, Tokenize
 app = Flask(__name__)
+
+@app.route('/tokenize_tekst', methods=['POST'])
+def tokenize_tekst():
+    if request.method == 'POST':
+        post = request.get_json()
+        
+        try:
+            T = Tokenize()
+            text = post["search_string"]
+            sent_tokenized_text = T.tokenize_raw_text_data(text)
+        except Exception as E:
+            raise E
+        
+        return jsonify({"tknz_txt": sent_tokenized_text})
 
 @app.route('/embed_string', methods=['POST'])
 def embed_string():
     if request.method == 'POST':
         post = request.get_json()
-                
+        
         try:
-            CE = CleanAndEmb(post["search_string"])
-            CE.clean_and_embed()
-            embedding = CE.embedding
+            CE = CleanAndEmb()
+            text = post["search_string"]
+            embedding = CE.clean_and_embed(text)
         except Exception as E:
             raise E
         
-        return jsonify({"emb":embedding.tolist()})
+        return jsonify({"emb": embedding.tolist()})
 
 @app.route('/dim_reduce', methods=['POST'])
 def dim_reduce():
@@ -22,28 +37,29 @@ def dim_reduce():
         post = request.get_json()
         
         try:
-            RD = ReduceDim(post["embedding"])
-            RD.reduce_dim()
-            reduced_emb = RD.emb100
+            RD = ReduceDim()
+            embedding = post["emb"]
+            if type(embedding) is list:
+                embedding = np.array(embedding)
+            reduced_embedding = RD.reduce_dim(embedding)
         except Exception as E:
             raise E
         
-        return jsonify({"emb100":reduced_emb.tolist()})
-    
+        return jsonify({"emb100": reduced_embedding.tolist()})
+
 @app.route('/dim_reduce_and_emb', methods=['POST'])
 def embed_and_reduce():
         if request.method == 'POST':
             post = request.get_json()
-            
+        
         try:
-            ER = EmbAndReduce(post["search_string"])
-            ER.clean_and_embed()
-            ER.reduce_dim()
-            reduced_emb = ER.emb100
+            ER = EmbAndReduce()
+            text = post["search_string"]
+            reduced_embedding = ER.embed_and_reduce(text)
         except Exception as E:
             raise E
         
-        return jsonify({"emb100":reduced_emb.tolist()})
+        return jsonify({"emb100": reduced_embedding.tolist()})
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug='True')
